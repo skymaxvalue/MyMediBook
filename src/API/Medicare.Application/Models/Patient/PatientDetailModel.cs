b1c2690cc6f2a0b3ad88ab11fa5c0cc2428a9a44
harshiit1 <bhardwajharshit171@gmail.com>
2026-06-08 16:55:31 +0530

    Add PhoneCountryCode property to PatientDetailModel

--- src/API/Medicare.Application/Models/Patient/PatientDetailModel.cs
+++ src/API/Medicare.Application/Models/Patient/PatientDetailModel.cs
@@ -7,6 +7,7 @@
         public string? MiddleName { get; set; }
         public string LastName { get; set; }
         public DateTime DateOfBirth { get; set; }
+        public string PhoneCountryCode { get; set; }
         public string PhoneNumber { get; set; }
         public string Email { get; set; }
         public string? Gender { get; set; }
