b1945afb96c1d0dd74b7465db03b865cb3c1e089
harshit <developer2@wintechsolutions.net>
2026-08-12 21:52:19 +0530

    Add CreateLabResult endpoint and related functionality
    Added a new CreateLabResult endpoint in LabController.cs to handle
    lab result creation. Introduced CreateLabResultCommand and its
    handler for MediatR integration. Updated ILabRepository and
    implemented CreateLabResultAsync in LabRepository.cs with database
    interaction and error handling. Created LabResultModel to define
    the data structure for lab results.

---
+++ src/API/Medicare.Application/Models/Lab/LabResultModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Medicare.Application.Models.Lab
+{
+    public class LabResultModel
+    {
+        public int OrderTestId { get; set; }
+        public List<ComponentResultDto> ComponentResults { get; set; }
+    }
+    public record ComponentResultDto
+    {
+        [JsonPropertyName("ComponentId")]
+        public int ComponentId { get; init; }
+
+        [JsonPropertyName("ResultValue")]
+        public string ResultValue { get; init; }
+
+        [JsonPropertyName("Interpretation")]
+        public string Interpretation { get; init; }
+
+        [JsonPropertyName("Comments")]
+        public string? Comments { get; init; }
+
+        [JsonPropertyName("Status")]
+        public string Status { get; init; }
+    }
+
+}
